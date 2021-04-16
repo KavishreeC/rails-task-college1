e36f938f9c139fa1f74e142f03eb394d91cab6ae
KavishreeC <kavishree.c@tringapps.com>
2021-04-16 15:37:42 +0530

    sidekiq config added

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,7 @@
+Sidekiq.configure_server do |config|
+    config.redis = { url: "radis//localhost:3000/0" }
+end
+
+Sidekiq.configure_client do |config|
+    config.redis = { url: "radis//localhost:3000/0" }
+end
